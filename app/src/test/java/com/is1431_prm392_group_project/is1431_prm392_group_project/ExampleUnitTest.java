0543e7baa414bff8a258331256dbb3b81ff354f6
ducnguyenedu <duc.nguyen.education1@gmail.com>
2022-05-18 08:28:12 +0700

    create project

---
+++ app/src/test/java/com/is1431_prm392_group_project/is1431_prm392_group_project/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.is1431_prm392_group_project.is1431_prm392_group_project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
