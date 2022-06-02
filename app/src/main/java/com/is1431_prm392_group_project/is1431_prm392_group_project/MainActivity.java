80a15ad8721386545cfff7d206b043b8ef9a4fa5
Sotatek-DucNguyen6 <duc.nguyen6@sotatek.com>
2022-06-02 17:00:53 +0700

    demo

--- app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/MainActivity.java
+++ app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/MainActivity.java
@@ -2,32 +2,30 @@ package com.is1431_prm392_group_project.is1431_prm392_group_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.is1431_prm392_group_project.is1431_prm392_group_project.dao.Repo;
 import com.is1431_prm392_group_project.is1431_prm392_group_project.models.entity.user.User;
+import com.is1431_prm392_group_project.is1431_prm392_group_project.modules.auth.controller.AuthController;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtOutput;
     Repo repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+super.onCreate(savedInstanceState);
+
         txtOutput = (TextView) findViewById(R.id.output);
 
         repo = new Repo(this);
 
         User user = new User("Bruno", "Shadow", "MySecretPassword",
                 "email@gmail.com");
-
-        txtOutput.append("Creating User: " + user.getAlias());
         user.save(repo);
-        txtOutput.append("\nUser: " + user.getAlias() + " created successfully!");
-        user = repo.Users.getByUsername("Shadow");
-        txtOutput.append("\nUser: " + user.getAlias() + " retrieved successfully!");
-        user.delete(repo);
-        txtOutput.append("\nUser: " + user.getAlias() + " deleted successfully!");
+
+        Intent i = new Intent(getApplicationContext(), AuthController.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/auth/controller/AuthController.java
+++ app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/auth/controller/AuthController.java
@@ -1,14 +1,30 @@
 package com.is1431_prm392_group_project.is1431_prm392_group_project.modules.auth.controller;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AuthController extends AppCompatActivity {
+import com.is1431_prm392_group_project.is1431_prm392_group_project.R;
+import com.is1431_prm392_group_project.is1431_prm392_group_project.dao.Repo;
+import com.is1431_prm392_group_project.is1431_prm392_group_project.models.entity.user.User;
 
+public class AuthController extends AppCompatActivity {
+    TextView txtOutput;
+    Repo repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
+        repo = new Repo(this);
+       User  user = repo.Users.getByUsername("Shadow");
+         txtOutput = (TextView) findViewById(R.id.output);
+        txtOutput.append("Creating User: " + user.getAlias());
+
+        txtOutput.append("\nUser: " + user.getAlias() + " created successfully!");
+        user = repo.Users.getByUsername("Shadow");
+        txtOutput.append("\nUser: " + user.getAlias() + " retrieved successfully!");
+        user.delete(repo);
+        txtOutput.append("\nUser: " + user.getAlias() + " deleted successfully!");
     }
 }
