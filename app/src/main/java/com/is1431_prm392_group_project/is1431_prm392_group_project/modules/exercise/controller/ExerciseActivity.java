01dd2182c1c0b2a5008af89678293be976243bd2
Sotatek-DucNguyen6 <duc.nguyen6@sotatek.com>
2022-06-20 20:34:09 +0700

    commit exercise

---
+++ app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/exercise/controller/ExerciseActivity.java
@@ -0,0 +1,16 @@
+package com.is1431_prm392_group_project.is1431_prm392_group_project.modules.exercise.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.is1431_prm392_group_project.is1431_prm392_group_project.R;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+    }
+}
