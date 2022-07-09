a2ce0b857fd2b2c27676e1ce9ce6d3bc0c22f209
ducnguyenedu <105717370+ducnguyenedu@users.noreply.github.com>
2022-07-09 14:46:39 +0700

    add Food

--- app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/home/providers/IHomeProvider.java
+++ app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/home/providers/IHomeProvider.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.is1431_prm392_group_project.is1431_prm392_group_project.dao.Repo;
 import com.is1431_prm392_group_project.is1431_prm392_group_project.entity.exercise.ExerciseList;
+import com.is1431_prm392_group_project.is1431_prm392_group_project.entity.food.Food;
 
 import java.util.ArrayList;
 
@@ -19,9 +20,32 @@ public class IHomeProvider {
         ExerciseList e1 = new ExerciseList();
         e1.setName("e1");
         ExerciseList e2 = new ExerciseList();
-        e1.setName("e2");
+        e2.setName("e2");
         ExerciseList e3 = new ExerciseList();
-        e1.setName("e3");
+        e3.setName("e3");
+        try {
+            e1.update(repo);
+            e2.update(repo);
+            e3.update(repo);
+            list.add(e1);
+            list.add(e2);
+            list.add(e3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public ArrayList<Food> getFoods() {
+        ArrayList<Food> list = new ArrayList<>();
+        Food e1 = new Food();
+        e1.setName("e1");
+        e1.setCalo(300);
+        Food e2 = new Food();
+        e2.setName("e2");
+        e2.setCalo(400);
+        Food e3 = new Food();
+        e3.setName("e3");
+        e3.setCalo(400);
         try {
             e1.update(repo);
             e2.update(repo);
