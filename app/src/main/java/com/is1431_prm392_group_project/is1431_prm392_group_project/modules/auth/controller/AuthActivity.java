b01d142568da575d70c70c05b2d10709625899c8
ducnguyenedu <105717370+ducnguyenedu@users.noreply.github.com>
2022-07-11 21:16:04 +0700

    commit

--- app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/auth/controller/AuthActivity.java
+++ app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/auth/controller/AuthActivity.java
@@ -3,22 +3,26 @@ package com.is1431_prm392_group_project.is1431_prm392_group_project.modules.auth
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 
+import com.is1431_prm392_group_project.is1431_prm392_group_project.R;
 import com.is1431_prm392_group_project.is1431_prm392_group_project.databinding.ActivityAuthBinding;
 import com.is1431_prm392_group_project.is1431_prm392_group_project.modules.auth.providers.AuthProvider;
-import com.is1431_prm392_group_project.is1431_prm392_group_project.R;
-public class AuthActivity extends AppCompatActivity {
+
+public class AuthActivity extends FragmentActivity {
     AuthProvider provider;
     private ActivityAuthBinding binding;
     private AppBarConfiguration appBarConfiguration;
+
     private void bindingInit() {
         binding = ActivityAuthBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_auth);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        provider = new AuthProvider(this);
     }
 
     private void bindingView() {
@@ -30,7 +34,6 @@ public class AuthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        provider = new AuthProvider(this);
         bindingInit();
         bindingView();
         bindingAction();
