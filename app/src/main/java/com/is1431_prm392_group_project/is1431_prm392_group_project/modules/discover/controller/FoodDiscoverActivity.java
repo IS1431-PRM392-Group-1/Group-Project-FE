2458b3794b1da975a39eec94884a4458283329e4
ducnguyenedu <105717370+ducnguyenedu@users.noreply.github.com>
2022-07-14 20:42:19 +0700

    add navigation for all

--- app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/discover/controller/FoodDiscoverActivity.java
+++ app/src/main/java/com/is1431_prm392_group_project/is1431_prm392_group_project/modules/discover/controller/FoodDiscoverActivity.java
@@ -1,17 +1,29 @@
 package com.is1431_prm392_group_project.is1431_prm392_group_project.modules.discover.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.is1431_prm392_group_project.is1431_prm392_group_project.R;
 import com.is1431_prm392_group_project.is1431_prm392_group_project.entity.food.Food;
+import com.is1431_prm392_group_project.is1431_prm392_group_project.modules.auth.controller.LoginActivity;
 import com.is1431_prm392_group_project.is1431_prm392_group_project.modules.discover.provider.FoodProvider;
+import com.is1431_prm392_group_project.is1431_prm392_group_project.modules.exercise.controller.ExerciseActivity;
+import com.is1431_prm392_group_project.is1431_prm392_group_project.modules.profile.controller.ProfileActivity;
+import com.is1431_prm392_group_project.is1431_prm392_group_project.modules.report.controller.ReportActivity;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -25,6 +37,10 @@ public class FoodDiscoverActivity extends AppCompatActivity {
     private FoodProvider provider;
     private Button reset;
     private TextView value;
+    private Toolbar toolbar;
+    private NavigationView nav;
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +52,50 @@ public class FoodDiscoverActivity extends AppCompatActivity {
     }
 
     private void OnBindingView() {
+        toolbar =
+                (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        nav =
+                (NavigationView) findViewById(R.id.nav_view2);
+        drawerLayout =
+                (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle =
+                new ActionBarDrawerToggle(
+                        this,
+                        drawerLayout,
+                        toolbar,
+                        R.string.Open,
+                        R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        nav.setNavigationItemSelectedListener(item -> onNavigationItemSelected(item));
         listview = this.findViewById(R.id.listView);
         calorieText = this.findViewById(R.id.calorieText);
         reset = this.findViewById(R.id.reset);
         value = this.findViewById(R.id.valueCal);
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_workout_list) {
+            Intent intent1 = new Intent(this, ExerciseActivity.class);
+            startActivity(intent1);
+        }
+        if (id == R.id.nav_profile_creation) {
+            Intent intent1 = new Intent(this, ProfileActivity.class);
+            startActivity(intent1);
+        }
+        if (id == R.id.nav_calo_calculated) {
+            Intent intent1 = new Intent(this, ReportActivity.class);
+            startActivity(intent1);
+        }
+        if (id == R.id.nav_login) {
+            Intent intent1 = new Intent(this, LoginActivity.class);
+            startActivity(intent1);
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private void OnBindingAction() {
